249d89bac92c89b79e75476d8913f1bb30b57c25
eya <eyagafsi17@gmail.com>
2023-12-31 17:03:52 +0100

    sprint 1

--- dist/flight.model.js
+++ dist/flight.model.js
@@ -5,11 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const mongoose_paginate_1 = __importDefault(require("mongoose-paginate"));
-//import AutoIncrement from 'mongoose-auto-increment';
-// Connect the mongoose-auto-increment plugin
-//AutoIncrement.initialize(mongoose.connection);
 let flightSchema = new mongoose_1.default.Schema({
-    // FNumber: { type: Number, required: true },
     duration: { type: String, required: true },
     date: { type: Date, required: true },
     returnDate: { type: Date, default: null },
@@ -21,6 +17,5 @@ let flightSchema = new mongoose_1.default.Schema({
     imagePath: { type: String, required: true },
 });
 flightSchema.plugin(mongoose_paginate_1.default);
-//flightSchema.plugin(AutoIncrement.plugin, { model: 'flight', field: 'FNumber', startAt: 1 });
 const flight = mongoose_1.default.model("flight", flightSchema);
 exports.default = flight;
